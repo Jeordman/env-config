20bb84e032f43bdf3353cacdd2cb3ef0e6786895
Jeordin Callister <Callisterjeordin@gmail.com>
2021-05-25 11:24:40 -0600

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+# FOR UBUNTU
+
+sudo apt install neovim
+
+# install plug for neovim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+       
+sudo apt install npm
+
+sudo apt install tmux
