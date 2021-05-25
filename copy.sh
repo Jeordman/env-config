c04729ac57d76f2d8e0963ef227a0a2b438095d7
Jeordin Callister <Callisterjeordin@gmail.com>
2021-05-25 12:02:17 -0600

    Update and rename move.sh to copy.sh

---
+++ copy.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+cp ./init.vim ~/.config/nvim/init.vim
+cp .tmux.conf ~/.tmux.conf
+cp .bashrc ~/.bashrc
+cp .bash_profile ~/.bash_profile
--- move.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-mv ./init.vim ~/.config/nvim/init.vim
-mv .tmux.conf ~/.tmux.conf
-mv .bashrc ~/.bashrc
-mv .bash_profile ~/.bash_profile
