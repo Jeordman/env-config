8f3c9a14a437eb4454bffd3d49e5db7102b6da45
Jeordin Callister <Callisterjeordin@gmail.com>
2021-05-26 21:13:38 -0600

    Update arch.sh

--- install/arch.sh
+++ install/arch.sh
@@ -4,6 +4,8 @@
 
 sudo pacman -S neovim
 
+sudo pacman -S neovim
+
 # install plug for neovim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
